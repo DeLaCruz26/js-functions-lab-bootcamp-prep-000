fa9eee6e80d0900cda8dec27be3d23a2fc7a0084
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-20 01:20:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
  return `Happy holidays, ${name}!`;
 }
-function happyHolidayTo(Independence, Kelsey) {
-  var holiday = "Independence Day";
-  var name = "Kelsey";
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday,days) {
